f39ee6666055828747c53b3dccdc06ddb7b731fa
gen BTC <genBTC@hushmail.com>
2016-05-13 00:38:54 -0400

    Redo GemDistribution to use normal BindableRichTextBox with Converter - to follow the rest of the program

---
+++ Procurement/View/Converters/GemDistributionStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Procurement.View
+{
+    public class GemDistributionStringConverter : IValueConverter 
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var all = value as string;
+            return new FlowDocument(new Paragraph(new Run(all) { Foreground = new SolidColorBrush { Color = Color.FromArgb(0xFF, 0xAB, 0x90, 0x66) }, FontWeight = FontWeights.Bold }));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
